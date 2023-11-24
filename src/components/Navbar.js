ae4eb05b292446025f6dbd95766be7611149eb38
QKL <quoclk2204@gmail.com>
2023-11-24 11:08:10 +0700

    new navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+          <div className="bg-blue-100">
+            <div className="h-14 px-12 flex items-center">
+              <p className="text font-bold">Employee management</p>
+            </div>
+          </div>
+          )
+}
+
+export default Navbar
