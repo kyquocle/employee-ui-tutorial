797f74ab543da1e4f0fc93f9c286251342abbd76
QKL <quoclk2204@gmail.com>
2023-11-24 11:08:28 +0700

    all files here

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import './App.css';
+import AddEmployee from './components/AddEmployee';
+import Navbar from './components/Navbar';
+import EmployeeList from "./components/EmployeeList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <BrowserRouter>
+      <Navbar/>
+
+      <Routes>
+
+        <Route path="/" element={<EmployeeList/>}></Route>
+        <Route index element={<EmployeeList/>}></Route>
+        <Route path="/add" element={<AddEmployee/>}></Route>
+
+      </Routes>
+      
+    </BrowserRouter>
+      
+    </>
+    
+  )
 }
 
 export default App;
